9a3617cda2c4c3afa0061d2c3c5c5f4bfe9bcf4a
NikitaRithe <nikitarithe2017@gmail.com>
2024-09-30 20:55:39 +0530

    hii

---
+++ src/Sample/demo1.java
@@ -0,0 +1,9 @@
+package Sample;
+
+public class demo1 
+{
+  public static void main(String[] args)
+  {
+	System.out.println("Demo 1 class created by user 1");
+  }
+}
---
+++ src/Sample/demo2.java
@@ -0,0 +1,9 @@
+package Sample;
+
+public class demo2 
+{
+  public static void main(String[] args) 
+  {
+	System.out.println("Demo 2 class created by user1");
+  }
+}
